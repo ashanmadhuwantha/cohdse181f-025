cf03ea33979360a9c36170d310890176cb1e6ebe
ashanmadhuwantha <39782724+ashanmadhuwantha@users.noreply.github.com>
2018-05-31 10:46:20 +0530

    image comment

---
+++ comment/image/com.php
@@ -0,0 +1,34 @@
+
+<html>
+
+<head>
+
+<title>Untitled Document</title>
+
+<style type="text/css">
+
+	body{border:solid black thick;width: 500px;height: 250px;}
+
+	</style>
+
+</head>
+
+<body>
+
+<form name="frmWorkBook" method="post" action="database.php">
+
+	<div id="frmguestcomment">
+
+		Please enter your comment:<br/>
+
+		<textarea name="comment" rows="5" cols="50"></textarea><br/><br/>
+
+		<input type="submit" name="send" value="send your comment"/>
+
+	</div>
+
+</form>
+
+</body>
+
+</html>
